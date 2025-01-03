f39ead203a42bc445cfe92657b4ff486d1633944
Shreyas-Yadav <shreyasyadav11@gmail.com>
2025-01-03 08:40:42 -0800

    add documentation and README for string conversion functions

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 def convert_to_snake_case(pascal_or_camel_cased_string):
+    """ use one of them to convert the string to snake case """
     # return usingLoop(pascal_or_camel_cased_string)
     return using_list_comprehension(pascal_or_camel_cased_string)
 
@@ -20,6 +21,20 @@ def using_list_comprehension(pascal_or_camel_cased_string):
     snake_cas_char_list = [ '_' + char.lower() if char.isupper() else char for char in pascal_or_camel_cased_string]
     return ''.join(snake_cas_char_list)
 
+
+
+"""
+    This module provides functions to convert PascalCase or camelCase strings to snake_case.
+    Functions:
+        convert_to_snake_case(pascal_or_camel_cased_string):
+            Converts a PascalCase or camelCase string to snake_case using the specified method.
+        usingLoop(pascal_or_camel_cased_string):
+            Converts a PascalCase or camelCase string to snake_case using a loop.
+        using_list_comprehension(pascal_or_camel_cased_string):
+            Converts a PascalCase or camelCase string to snake_case using list comprehension.
+        main():
+            Demonstrates the conversion of a PascalCase string to snake_case by printing the result.
+"""
 def main():
     print(convert_to_snake_case('IAmAPascalCasedString'))
 
---
+++ readme.md
@@ -0,0 +1,3 @@
+List Comprehension is a way to construct a new Python list from an iterable types: lists, tuples, and strings. All without using a for loop or the `.append()` list method.
+
+In this project,program that takes a camelCase or PascalCase formatted string as input and converts that to a snake_case formatted string using two approaches. First, for loop and then list comprehension to achieve the same results. You'll see how list comprehension can make your code more concise.
